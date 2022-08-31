efa6b376d7f3327ff6e806371ccece7706041097
Meara1179 <xenon1179@gmail.com>
2022-09-01 00:10:30 +1000

    Complete

--- KiddEsports/MainWindow.xaml.cs
+++ KiddEsports/MainWindow.xaml.cs
@@ -142,14 +142,13 @@ namespace KiddEsports
             System.Windows.Application.Current.Shutdown();
         }
         #endregion
-        #region Helpers
-        // Method which gets the team objects from a file and uses it to populate the data grid.
+        #region Methods
         private void SetupDataGrid()
         {
             teamList = file.GetTeam();
             dgvTeamList.ItemsSource = teamList;
         }
-        // Creates a new TeamDetails object when called.
+
         private void SaveNewEntry()
         {
             team.TeamName = txtTeamName.Text;
@@ -162,6 +161,12 @@ namespace KiddEsports
             teamList = file.GetTeam();
             dgvTeamList.Items.Refresh();
         }
+        #endregion
+        #region Helpers
+        // Method which gets the team objects from a file and uses it to populate the data grid.
+
+        // Creates a new TeamDetails object when called.
+
         // Clears the data entry fields when called.
         private void ClearDataEntryField()
         {
